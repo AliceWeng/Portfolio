7ad266459bf831e3414a0f7e858e28d7a579b61f
Alice <alicewengs@gmail.com>
2022-11-16 15:11:13 -0800

    added a cloud and bubble pop sound effect

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let sun = document.querySelector(".sun");
 sun.addEventListener("click", () => {
     document.body.classList.toggle("night");
 });
+
+let bubble = document.querySelector(".bubble");
+let pop = new Audio("./audio/pop.mp3");
+bubble.onclick = () => pop.play();
