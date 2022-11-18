a1f218bd8ce9ab7a0f17d7db8242871fa4233d3d
Alice <alicewengs@gmail.com>
2022-11-18 11:52:27 -0800

    added animated bubbles that pop onclick

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 let sun = document.querySelector(".sun");
+
 sun.addEventListener("click", () => {
     document.body.classList.toggle("night");
 });
 
-let bubble = document.querySelector(".bubble");
+let main = document.querySelector("main");
 let pop = new Audio("./audio/pop.mp3");
-bubble.onclick = () => pop.play();
+
+let createBubble = () => {
+    let bubble = document.createElement("div");
+    bubble.className = "bubble";
+    let moveBubble = () => {
+        bubble.style.left = Math.floor(Math.random() * ((document.body.clientWidth - 50) - 50 + 1) + 50) + "px";
+    }
+    moveBubble();
+    let clicked = false;
+    bubble.addEventListener("click", () => {
+        clicked = true;
+        pop.play();
+        bubble.remove();
+        createBubble();
+    });
+    setTimeout(() => {
+        if(!clicked) {
+            moveBubble();
+        }
+    }, 8000);
+    main.append(bubble);
+}
+
+for(let i = 0; i < 5; i++) {
+     setTimeout(() => {
+        createBubble();
+    }, i * 1600)
+}
+
