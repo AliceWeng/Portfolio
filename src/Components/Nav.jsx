0b5c10848d9b4d0f3fb88221ec9cb7598b347fe6
Alice <alicewengs@gmail.com>
2023-06-15 23:33:23 -0700

    added focus trap for nav menu

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { HashLink } from "react-router-hash-link";
 
 function Nav() {
     const [open, setOpen] = useState(false);
 
+    const hamburgerRef = useRef();
+
     return (
         <nav>
             <div className="bar">
-                <button aria-label={open ? "close menu" : "open menu"} className={`${open ? "close" : "open"} hamburger`} onClick={() => setOpen(!open)}></button>
+                <button
+                    className={`${open ? "close" : "open"} hamburger`}
+                    aria-label={open ? "close menu" : "open menu"}
+                    onClick={() => setOpen(!open)}
+                    ref={hamburgerRef}>
+                </button>
             </div>
-            <ul className={!open && "hidden"}>
+            <ul className={open ? null : "hidden"}>
                 <li>
                     <HashLink to="#about" onClick={() => setOpen(false)} smooth>About</HashLink>
                 </li>
@@ -19,6 +26,8 @@ function Nav() {
                 <li>
                     <HashLink to="#projects" onClick={() => setOpen(false)} smooth>Projects</HashLink>
                 </li>
+                {/* Found a solution for focus trap. - https://stackoverflow.com/a/75300986 */}
+                <span tabIndex="0" aria-hidden="true" onFocus={() => hamburgerRef.current.focus()}></span>
             </ul>
         </nav>
     )
